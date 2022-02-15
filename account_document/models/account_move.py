1bc82f70b3350250c06662f108137cade110c099
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-14 20:54:43 -0500

    Update account_move.py

--- account_document/models/account_move.py
+++ account_document/models/account_move.py
@@ -10,4 +10,13 @@ class AccountMove(models.Model):
     document_number = fields.Char(
         string='Document Number')
     is_expo = fields.Boolean(
-        string="Is expo")
+        string="Is expo",
+        compute="_compute_is_expo")
+
+    @api.depends('document_type_id')
+    def _compute_is_expo(self):
+        for record in self:
+            if document_type_id.name == 'FACTURAS DE EXPORTACION':
+                record.is_expo = True
+            else:
+                record.is_expo = False

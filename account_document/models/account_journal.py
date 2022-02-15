e53012eb5825a8235459002644ef71ba4fdf078d
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-14 20:46:05 -0500

    add b

--- account_document/models/account_journal.py
+++ account_document/models/account_journal.py
@@ -4,14 +4,10 @@ class AccountJournalDocumentType(models.Model):
     _name = "account.journal.document.type"
     _description = "Journal Document Types Mapping"
     _rec_name = 'document_type_id'
-    _order = 'journal_id desc, sequence, id'
 
     document_type_id = fields.Many2one(
         'account.document.type',
         'Document Type')
     journal_id = fields.Many2one('account.journal')
-    sequence = fields.Integer(
-        'Sequence',
-        index=True,
-    )
+    sequence = fields.Integer()
     next_number = fields.Integer()

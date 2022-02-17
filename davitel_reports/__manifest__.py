480639c76f5dcd4b9244c5a9194e0073efdb4119
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-17 12:22:53 -0500

    add

---
+++ davitel_reports/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Davitel reports",
+
+    'summary': """
+        stock""",
+
+    'author': "NybbleGroup",
+
+    'website': "",
+
+    'contributors': ['NybbleGroup'],
+
+    'category': 'reports',
+
+    'version': '14.0.0.1',
+
+    'depends': [
+        'stock',
+    ],
+
+    'data': [
+
+        #"views/stock_picking_type.xml",
+      
+    ],
+    
+    'installable': True,
+}
---
+++ davitel_reports/models/account_move.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+class AccountMove(models.Model):
+    _inherit = 'account_move'
